d05a62af7047925d3232e1d8edc12811eed22919
theplat <109287817+theplat@users.noreply.github.com>
2024-07-08 14:35:58 -0400

    Update and rename hddenc.info to hddenc.sh

--- hddenc.sh
+++ hddenc.sh
@@ -1,4 +1,7 @@
-DEVICE="/dev/sdX"
+#!/usr/bin/env bash
+
+DEVICE=$1
 PASS=$(tr -cd '[:alnum:]' < /dev/urandom | head -c128)
 sudo openssl enc -aes-256-ctr -pbkdf2 -iter 10000 -pass pass:"$PASS" -nosalt </dev/zero | sudo dd obs=64K ibs=4K of=$DEVICE oflag=direct status=progress
-dd if=/dev/zero of=$DEVICE bs=1M status=progress
+sudo dd if=/dev/zero of=$DEVICE bs=1M status=progress
+PASS="12345"
