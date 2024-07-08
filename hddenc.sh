0a12c2e0de9373ca79ce4362d4e807248fa2cb3a
theplat <109287817+theplat@users.noreply.github.com>
2024-07-08 14:39:45 -0400

    Update hddenc.sh

--- hddenc.sh
+++ hddenc.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+if [ -z "$1" ]
+then
+  echo "Quitting, you didnt specify a device"
+  exit 1
+fi
+
 DEVICE=$1
 PASS=$(tr -cd '[:alnum:]' < /dev/urandom | head -c128)
 sudo openssl enc -aes-256-ctr -pbkdf2 -iter 10000 -pass pass:"$PASS" -nosalt </dev/zero | sudo dd obs=64K ibs=4K of=$DEVICE oflag=direct status=progress
